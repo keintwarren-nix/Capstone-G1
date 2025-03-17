94f28eb67e1711f4124bba1fcca744aac400c0ae
Keint Warren <kwpoliquit@gmail.com>
2025-03-17 16:08:34 +0800

    1st try

---
+++ src/main/Ui.java
@@ -0,0 +1,12 @@
+package main;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
+
+
+public class Ui {
+    GamePanel gp;
+    Graphics2D g2;
+
+    BufferedImage cinderIcon;
+}
